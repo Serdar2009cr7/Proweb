6be09067577d29107f4857dfb11eac4065a859ad
Serdar2009cr7 <rowsennobatow2@gmail.com>
2025-05-13 20:02:12 +0500

    first commit

---
+++ tests/test_login.py
@@ -0,0 +1,24 @@
+import pytest
+
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
+
+def test_login_success(driver):
+    driver.get("https://my.proweb.uz/log-in")
+
+    login = LoginPage(driver)
+    login.enter_phone_number("998335814130")
+    login.click_login()
+    login.enter_password("proweb3106632")
+    login.click_login()
+
+
+
+# def test_login_invalid(driver):
+#     driver.get("https://my.proweb.uz/log-in")
+#
+#     login = LoginPage(driver)
+#     login.enter_phone_number("998335814131")
+#     login.click_login()
+#     login.enter_password("proweb3106632")
+#     login.click_login()
